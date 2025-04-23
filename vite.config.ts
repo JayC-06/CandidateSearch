cda427b8ca12275ca7c6ad15e5a876a9774af427
JayC-06 <Contreras.josh06@gmail.com>
2025-04-22 22:57:31 -0400

    added new link to vite

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Bind to 0.0.0.0 to listen on all interfaces (needed for Render)
     port: Number(process.env.PORT) || 3000, // Use the PORT provided by Render, fallback to 3000
-    allowedHosts: ['https://candidatesearch-i7gs.onrender.com/'], // Add this host to allowed hosts
+    allowedHosts: ['candidatesearch-i7gs.onrender.com'], // Add this host to allowed hosts
   },
 });
